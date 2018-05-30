95e474e9b5b6bb9c86844fe182506c10740069f6
Leon <ultrafunkamsterdam@users.noreply.github.com>
2018-05-30 16:38:50 +0200

    Update recipe-412551.py
    more simple addition

--- recipes/Python/412551_Simple_Singleton/recipe-412551.py
+++ recipes/Python/412551_Simple_Singleton/recipe-412551.py
@@ -20,3 +20,45 @@ tb1, tb2 = Test(5), Test(5)
 assert tb1 is tb2
 
 assert ta1 is not tb1
+
+
+
+
+
+# simpler:
+
+def singleton(cls):
+    cls._instance = cls._instance or cls()
+    return cls._instance
+ 
+ class ThreadKiller:
+    _instance = None
+    active = False
+
+tk1 = singleton(ThreadKiller)
+tk2 = singleton(ThreadKiller)
+tk3 = singleton(ThreadKiller)
+
+tk1.active
+# False
+tk2.active
+# False
+tk3.active
+# False
+
+tk3.active = True
+tk.active
+# True
+tk2.active
+# True
+tk1.active
+# True
+
+print(tk1 is tk2 is tk3)
+True
+
+tk, tk1, tk2, tk3
+#<__main__.ThreadKiller at 0x7f8578bd6fd0>,
+#<__main__.ThreadKiller at 0x7f8578bd6fd0>,
+#<__main__.ThreadKiller at 0x7f8578bd6fd0>,
+#<__main__.ThreadKiller at 0x7f8578bd6fd0>
